945f383b488d72152133fe6453672159bc7578e6
Tkzo <contact@tkzo.jp>
2023-02-06 23:34:46 +0900

    0.4.0

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,5 +2,6 @@
  * Public API Surface of ngx-tiny-carousel
  */
 
+export * from './lib/component/ngx-tiny-carousel-cell/ngx-tiny-carousel-cell.component';
 export * from './lib/ngx-tiny-carousel.component';
 export * from './lib/ngx-tiny-carousel.module';
