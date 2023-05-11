9c400f57052afb2cb1cbe10c16e84406f359c6f7
Tkzo <contact@tkzo.jp>
2023-05-11 21:48:09 +0900

    Modified public-api.ts

--- src/public-api.ts
+++ src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/component/ngx-tiny-carousel-cell/ngx-tiny-carousel-cell.component';
 export * from './lib/ngx-tiny-carousel.component';
 export * from './lib/ngx-tiny-carousel.module';
+export * from './lib/directive/lazy-content.directive';
